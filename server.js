1146b66b82a02e491c0ea27f5e4a279e9e25f804
Aleksey Korovin <aleksey.korovin@valtech.com>
2015-02-19 18:06:24 +0200

    update

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    app = express();
+
+//Express 3
+//app.configure(function() {
+//    app.use(express.static(__dirname, '/'));
+//});
+
+//Express 4
+app.use(express.static(__dirname, '/'));
+
+app.listen(8080);
+
+console.log('Express listening on port 8080');
